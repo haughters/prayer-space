e7bdd49e8392bc07a3ffda26990cc8ba7df6e9af
Jamie Haughton <jamieh911@hotmail.co.uk>
2026-07-15 20:36:27 +0100

    fix(services): conditionally load RestTemplateConfig to fix GraalVM crash in group-service

--- services/common/src/main/java/com/prayerlink/common/config/RestTemplateConfig.java
+++ services/common/src/main/java/com/prayerlink/common/config/RestTemplateConfig.java
@@ -2,10 +2,8 @@ package com.prayerlink.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
 public class RestTemplateConfig {
 
     @Bean
--- services/identity-service/src/main/java/com/prayerlink/identity/IdentityApplication.java
+++ services/identity-service/src/main/java/com/prayerlink/identity/IdentityApplication.java
@@ -2,8 +2,11 @@ package com.prayerlink.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import com.prayerlink.common.config.RestTemplateConfig;
 
 @SpringBootApplication(scanBasePackages = "com.prayerlink")
+@Import(RestTemplateConfig.class)
 public class IdentityApplication {
   public static void main(String[] args) {
     SpringApplication.run(IdentityApplication.class, args);
